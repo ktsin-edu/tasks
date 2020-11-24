87a128ec0c5cf76c72ed262bac2033dec749e615
Pavel Kazutsin <kazyten@ya.ru>
2020-11-24 03:15:54 +0300

    JSON writer prototype

---
+++ task1/Writer/IDataWriter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1.Writer
+{
+    public interface IDataWriter
+    {
+        public bool Open(object path);
+        public bool Write(Reader.IDataCollector collector);
+    }
+}
---
+++ task1/Writer/JSONWriter.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task1.Reader;
+
+namespace task1.Writer
+{
+    public class JSONWriter : IDataWriter
+    {
+        public JSONWriter()
+        {
+            settings.TypeNameHandling = TypeNameHandling.Objects;
+        }
+
+        public bool Open(object path)
+        {
+            this.path = path as string;
+            if (this.path != null)
+                return File.Exists(this.path);
+            else
+                return false;
+        }
+
+        public bool Write(IDataCollector collector)
+        {
+            try
+            {
+                var src = collector.GetSource();
+                var jsonString = JsonConvert.SerializeObject(src);
+                var fileStream = File.Open(this.path, FileMode.OpenOrCreate);
+                fileStream.Write(Encoding.Default.GetBytes(jsonString));
+                fileStream.Flush();
+                fileStream.Close();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Debug.Print(ex.Message);
+                return false;
+            }
+        }
+
+        private string path;
+        private readonly JsonSerializerSettings settings;
+    }
+}
